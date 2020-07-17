35376a2ba61c84fbd412eddd3906fc500aa29c1f
allenlin90 <ht016746@gmail.com>
2020-07-17 13:15:21 +0700

    update NodeJS learning note. Finish learning Asynchronous NodeJS

--- javascriptLearning/theCompleteNodejsDeveloperCourse/sandbox/6-raw-http.js
+++ javascriptLearning/theCompleteNodejsDeveloperCourse/sandbox/6-raw-http.js
@@ -1,6 +1,7 @@
 const http = require("http");
 
 const url = `http://api.weatherstack.com/current?access_key=//&query=40,-75`;
+// Get access token from weather stack dashboard
 
 const request = http.request(url, (response) => {
   let data = "";
