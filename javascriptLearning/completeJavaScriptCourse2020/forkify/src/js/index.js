8deebc5227a72b59b7b845ee507c4fda9efc71f9
allenlin90 <ht016746@gmail.com>
2020-07-20 22:44:55 +0700

    update js learning note. Start calling forkify-api with async and await with axios package

--- javascriptLearning/completeJavaScriptCourse2020/forkify/src/js/index.js
+++ javascriptLearning/completeJavaScriptCourse2020/forkify/src/js/index.js
@@ -1,4 +1,16 @@
-// Global app controller
-import num from "./test";
-const x = 23;
-console.log(`I imported ${num} from another module! Variable x is ${x}`);
+import axios from "axios";
+
+async function getResults(query) {
+  try {
+    const result = await axios(
+      `https://forkify-api.herokuapp.com/search?q=${query}`
+    );
+    const recipes = result.data.recipes;
+    console.log(recipes);
+  } catch (error) {
+    alert(error);
+  }
+}
+
+getResults("pizza");
+getResults(encodeURIComponent("sweet potato"));
