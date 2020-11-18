e84ef4034376232d15f6033ef6ae359befb65976
allenlin90 <ht016746@gmail.com>
2020-11-18 20:33:43 +0700

    try qrcodejs in a4 printing layout

--- sandbox/a4printing/app.js
+++ sandbox/a4printing/app.js
@@ -1,1 +1,6 @@
-console.log('Connected');
+var typeNumber = 0;
+var errorCorrectionLevel = 'L';
+var qr = qrcode(typeNumber, errorCorrectionLevel);
+qr.addData('https://app.makesend.asia/');
+qr.make();
+document.getElementById('qr_code_img').innerHTML = qr.createImgTag(8, 0, 'qr_code');
