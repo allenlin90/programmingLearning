a54dd44cbc68040335a936fd7e773254e4301e25
allenlin90 <ht016746@gmail.com>
2020-12-08 17:30:07 +0700

    update receipt layout for printing

--- sandbox/thermalReceipt/app.js
+++ sandbox/thermalReceipt/app.js
@@ -1,6 +1,20 @@
+makeQRCode(`Invoice ID: INV2011102018985`);
+function makeQRCode(content = '') {
+    const typeNumber = 0;
+    const errorCorrectionLevel = 'M';
+    const qr = qrcode(typeNumber, errorCorrectionLevel);
+    qr.addData(content);
+    qr.make();
+    document.getElementById('qr_code').innerHTML = qr.createImgTag(4, 0, 'qr_code');
+}
+
 const printButton = document.getElementById('printButton')
 printButton.addEventListener('click', () => {
+    let now = new Date();
+    let today = `${now.getDate()}/${now.getMonth() + 1}/${now.getFullYear()} - ${now.getHours()}:${now.getMinutes()}`;
     printButton.style.display = 'none';
+    document.getElementById('printedTime').innerText = today;
     window.print();
     printButton.style.display = 'block';
+    document.getElementById('printedTime').innerText = '';
 });
